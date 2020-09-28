2224936d5f1da49394ccb2c8dcf63da0375d0e6f
Mateusz Wszola <wszola.m@gmail.com>
2020-09-28 23:32:13 +0200

    add grid layout around filters

--- frontend/components/Issues.js
+++ frontend/components/Issues.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Heading, Box, Flex, Stack } from '@chakra-ui/core';
+import { Heading, Box, Flex, SimpleGrid } from '@chakra-ui/core';
 import { InputSearch } from '@/components/InputSearch';
 import { FilterMenu } from '@/components/issues/FilterMenu';
 
@@ -10,32 +10,34 @@ export const Issues = () => {
         <Heading size="lg">Issues</Heading>
       </Box>
 
-      <Flex mt={4} direction={['column', null, 'row']} align="center">
+      <Flex mt={4} direction={['column', null, 'row']} align={{ sm: 'center' }}>
         <Box w="full" maxW={['100%', 'xs']}>
           <InputSearch />
         </Box>
 
-        <Stack
+        {/* <Stack
           direction="row"
           align="center"
           flexWrap="wrap"
-          spacing={{ base: 0, md: 2 }}
+          spacing={{ base: 0, sm: 1, md: 2 }}
           ml={{ md: 4 }}
           mt={{ base: 2, md: 0 }}
-        >
-          <Box flex="1">
+        > */}
+        <SimpleGrid mt={{ base: 2, md: 0 }} ml={{ md: 4 }} columns={[2, 4]} spacing={1}>
+          <Box>
             <FilterMenu label="Assignee" options={['User #1', 'User #2']} />
           </Box>
-          <Box flex="1">
+          <Box>
             <FilterMenu label="Priority" options={['P1', 'P2', 'P3', 'P4', 'P5']} />
           </Box>
-          <Box flex="1">
+          <Box>
             <FilterMenu label="Status" options={['To Do', 'In Progress', 'Done']} />
           </Box>
-          <Box flex="1">
+          <Box>
             <FilterMenu label="Type" options={['Task', 'Bug', 'Feature Request']} />
           </Box>
-        </Stack>
+        </SimpleGrid>
+        {/* </Stack> */}
       </Flex>
     </>
   );
--- frontend/components/issues/FilterMenu.js
+++ frontend/components/issues/FilterMenu.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import {
   Button,
@@ -11,13 +12,19 @@ import {
 import { GoChevronDown } from 'react-icons/go';
 
 export const FilterMenu = ({ label, options }) => {
+  const [numberOfFilters, setNumberOfFilters] = React.useState(0);
+
+  const handleChange = (newValues) => {
+    setNumberOfFilters(newValues.length);
+  };
+
   return (
     <Menu closeOnSelect={false}>
       <MenuButton as={Button} size="sm" variant="ghost" rightIcon={<Icon as={GoChevronDown} />}>
-        {label}
+        {label} {numberOfFilters > 0 ? `(${numberOfFilters})` : ''}
       </MenuButton>
       <MenuList>
-        <MenuOptionGroup type="checkbox">
+        <MenuOptionGroup type="checkbox" onChange={handleChange}>
           {options.map((option) => (
             <MenuItemOption key={option} value={option}>
               {option}
