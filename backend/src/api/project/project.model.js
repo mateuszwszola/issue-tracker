33e85fe1a96172d5496584e718c7777894d5a7bf
Mateusz Wszola <wszola.m@gmail.com>
2020-10-20 23:17:18 +0200

    add project model

--- backend/src/api/project/project.model.js
+++ backend/src/api/project/project.model.js
@@ -5,6 +5,20 @@ class ProjectModel extends Model {
   static get tableName() {
     return tableNames.project;
   }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['key', 'name', 'owner_id'],
+
+      properties: {
+        id: { type: 'integer' },
+        key: { type: 'string', minLength: 1, maxLength: 255 },
+        name: { type: 'string', minLength: 1, maxLength: 255 },
+        description: { type: 'string', minLength: 1, maxLength: 255 },
+      },
+    };
+  }
 }
 
 export { ProjectModel };
