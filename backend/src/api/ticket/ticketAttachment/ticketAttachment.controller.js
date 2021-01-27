5d921a82df4e715ac1c4be14f5533f3998ae080f
Mateusz Wszola <wszola.m@gmail.com>
2021-01-27 23:15:37 +0100

    Upload attachment to S3

--- backend/src/api/ticket/ticketAttachment/ticketAttachment.controller.js
+++ backend/src/api/ticket/ticketAttachment/ticketAttachment.controller.js
@@ -3,12 +3,13 @@ import { nanoid } from 'nanoid';
 
 export const signTicketAttachment = async (req, res) => {
   const s3 = new aws.S3();
-  const fileName = req.query['file-name'];
   const fileType = req.query['file-type'];
+  // Generate random key for an image
+  const fileKey = nanoid();
 
   const s3Params = {
     Bucket: S3_BUCKET,
-    Key: fileName,
+    Key: fileKey,
     Expires: 120,
     ContentType: fileType,
     ACL: 'public-read',
@@ -21,7 +22,7 @@ export const signTicketAttachment = async (req, res) => {
       } else {
         resolve({
           signedRequest: data,
-          url: `https://${S3_BUCKET}.s3.amazonaws.com/${nanoid()}`,
+          url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileKey}`,
         });
       }
     });
--- frontend/components/issue/Attachments.js
+++ frontend/components/issue/Attachments.js
@@ -1,11 +1,106 @@
-import { Box, Text } from '@chakra-ui/react';
+import { useRef, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Box, Button, CircularProgress, Image, Text, useToast } from '@chakra-ui/react';
+import client from '@/utils/api-client';
+import { FaPlus } from 'react-icons/fa';
+
+function AddAttachment() {
+  const inputRef = useRef();
+  const toast = useToast();
+  const { getAccessTokenSilently } = useAuth0();
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const uploadFile = async (file, signedRequest, url) => {
+    try {
+      await fetch(signedRequest, { method: 'PUT', body: file });
+      toast({
+        title: 'Attachment uploaded.',
+        status: 'success',
+        duration: 3000,
+        isClosable: true
+      });
+      setImageUrl(url);
+      inputRef.current.value = '';
+    } catch (err) {
+      toast({
+        title: 'An error occurred.',
+        description: 'Unable to upload a file.',
+        status: 'error',
+        duration: 9000,
+        isClosable: true
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getSignedRequest = async (file) => {
+    setLoading(true);
+
+    try {
+      const token = await getAccessTokenSilently();
+
+      const { data } = await client(
+        `tickets/attachment/sign-s3?file-name=${encodeURIComponent(
+          file.name
+        )}&file-type=${encodeURIComponent(file.type)}`,
+        {
+          token
+        }
+      );
+
+      await uploadFile(file, data.signedRequest, data.url);
+    } catch (err) {
+      toast({
+        title: 'An error occurred.',
+        description: 'Unable to upload a file.',
+        status: 'error',
+        duration: 9000,
+        isClosable: true
+      });
+      setLoading(false);
+    }
+  };
+
+  const onFileChange = async () => {
+    const { files } = inputRef.current;
+    const file = files[0];
+    if (file) {
+      await getSignedRequest(file);
+    }
+  };
+
+  return (
+    <>
+      {loading && <CircularProgress isIndeterminate />}
+      <Box boxSize="100px">{imageUrl && <Image w="full" objectFit="cover" src={imageUrl} />}</Box>
+
+      <Button
+        mt={2}
+        onClick={() => inputRef.current.click()}
+        leftIcon={<FaPlus />}
+        variant="outline"
+        colorScheme="blue"
+        size="sm"
+      >
+        Upload photo
+      </Button>
+
+      <input id="fileUpload" ref={inputRef} type="file" onChange={onFileChange} hidden />
+    </>
+  );
+}
 
 function Attachments({ ...chakraProps }) {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <Box {...chakraProps}>
       <Text fontSize="sm" fontWeight="medium">
         Attachments 0
       </Text>
+      {isAuthenticated && <AddAttachment />}
     </Box>
   );
 }
